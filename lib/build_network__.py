e8cfb2b47c4d6c75e4e5746701f482ba882a9657
Eric Petersen <eric.alphagriffin@gmail.com>
2017-04-03 00:49:57 -0600

    added a trained model for testing

--- lib/build_network__.py
+++ lib/build_network__.py
@@ -27,14 +27,13 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 working_path = os.getcwd()
 
 
-class Build_Model(object):
+class BuildModel(object):
     def __init__(self, conv, fc, outputs):
         self.num_conv = conv
         self.num_fc = fc
         self.num_outputs = outputs
 
-    @staticmethod
-    def BUILD_CONV_LAYERS(x_image, layers, f_size=5):
+    def build_conv_layers(self, x_image, layers, f_size=5):
         """
         Magically construct many Convolutional layers for a Neural Network.
         :return:
@@ -51,7 +50,7 @@ class Build_Model(object):
             log.debug("Current Num of Features= {}".format(num_f))
             if layer == 0:
                 img = start_shape
-                channel = dataset_c
+                channel = x_image.shape[3]
             else:
                 img = reducing_shape
                 channel = last_num_f
@@ -59,10 +58,10 @@ class Build_Model(object):
             last_num_f = num_f
             log.debug("Start shape= {}".format(start_shape))
             debugs = "New Features = {}".format(channel)
-            reducing_shape, w, b = new_conv_layer(input=img,
-                                                  filter_size=f_size,
-                                                  chan=channel,
-                                                  num_filters=num_f)
+            reducing_shape, w, b = self.new_conv_layer(input=img,
+                                                      filter_size=f_size,
+                                                      chan=channel,
+                                                      num_filters=num_f)
             layer_name = "convLayer_%s" % layer
             log.debug("Finishing Layer {}\n\t{}".format(layer_name, debugs))
             # tf.summary.histogram("weights{}".format(layer), w)
@@ -72,8 +71,7 @@ class Build_Model(object):
         log.info("Finished Building {} Conv Layers\nMoving To Flattening...".format(layers))
         return reducing_shape, last_num_f
 
-    @staticmethod
-    def BUILD_FC_LAYER(x_image, num_fc, num_final):
+    def build_fc_layers(self, x_image, num_fc, num_final):
         """
         Magically create a huge number of Fully connected layers.
         :param layers:
@@ -81,7 +79,7 @@ class Build_Model(object):
         :return:
             returns a human readable non used list of the layer names. These are added to tensorboard automatically.
         """
-        log.info("Building {} Fully Connected Layers\ninput features size = {}\n*-".format(num_fc, in_features))
+        log.info("Building {} Fully Connected Layers".format(num_fc))
         # first time thru options
         inputs = 0
         features = 0
@@ -102,11 +100,11 @@ class Build_Model(object):
                 use_reLu = False  # dont use on the last time thru
                 use_Drop = False
 
-            current_layer, w, b = new_fc_layer(input_=current_layer,
-                                               num_inputs=inputs,
-                                               num_outputs=features,
-                                               use_relu=use_reLu,
-                                               use_drop=use_Drop)
+            current_layer, w, b = self.new_fc_layer(input_=current_layer,
+                                                    num_inputs=inputs,
+                                                    num_outputs=features,
+                                                    use_relu=use_reLu,
+                                                    use_drop=use_Drop)
 
             layer_name = "fullyLayer_{}".format(layer)
             with tf.name_scope(layer_name):
@@ -116,28 +114,28 @@ class Build_Model(object):
                     tf.summary.histogram('biases', b)
         return current_layer  # final layer
 
-    def new_fc_layer(input_, num_inputs, num_outputs, use_relu=True, use_drop=False):
-        weights = new_weights(shape=[num_inputs, num_outputs])  # set weights
-        biases = new_biases(length=num_outputs)  # set number of OUTPUTS like give me top k or whatever...
+    def new_fc_layer(self, input_, num_inputs, num_outputs, use_relu=True, use_drop=False):
+        weights = self.new_weights(shape=[num_inputs, num_outputs])  # set weights
+        biases = self.new_biases(length=num_outputs)  # set number of OUTPUTS like give me top k or whatever...
         layer = tf.matmul(input_, weights) + biases  # this is a #BIGMATH func
         if use_relu:
             layer = tf.nn.relu(layer)
         if use_drop:
-            layer = tf.nn.dropout(layer, keep_prob)
+            layer = tf.nn.dropout(layer, self.keep_prob)
         return layer, weights, biases
 
-    def new_weights(shape):
+    def new_weights(self, shape):
         """This generates a new weight for each layer"""
         return tf.Variable(tf.truncated_normal(shape, stddev=0.1), name="weight", collections=["weights"])
 
-    def new_biases(length):
+    def new_biases(self, length):
         """This generates a new bias for each layer"""
         return tf.Variable(tf.constant(0.1, shape=[length]), name="bias", collections=["biases"])
 
-    def new_conv_layer(input, filter_size, chan, num_filters, use_pooling=True):
+    def new_conv_layer(self, input, filter_size, chan, num_filters, use_pooling=False):
         X_shape = [filter_size, filter_size, chan, num_filters]
-        weights = new_weights(shape=X_shape)
-        biases = new_biases(length=num_filters)
+        weights = self.new_weights(shape=X_shape)
+        biases = self.new_biases(length=num_filters)
         """ THis is the MAGIC again... """
         layer = tf.nn.conv2d(input=input,  # This is the output of the last layer
                              filter=weights,  # this is a thing
@@ -154,7 +152,7 @@ class Build_Model(object):
         log.debug("Finished Building a conv Layer:\n\t{}".format(layer))
         return layer, weights, biases
 
-    def flatten_layer(layer):
+    def flatten_layer(self, layer):
         log.info("Tranitioning from Conv to fc layers")
         layer_shape = layer.get_shape()  # ASSERT layer_shape == [num_images, img_height, img_width, num_channels]
         num_features = layer_shape[1:4].num_elements()  # like a boss...
@@ -163,7 +161,7 @@ class Build_Model(object):
         log.info("Finished Flattening Layers")
         return layer_flat, num_features
 
-    def build_inputs():
+    def build_inputs(self, dataset_h, dataset_w, dataset_c, dataset_classes):
         with tf.variable_scope('inputs'):
             global_step = tf.Variable(0, trainable=False, name='global_step')
             learn_rate = tf.train.exponential_decay(0.1,
@@ -182,23 +180,23 @@ class Build_Model(object):
                                                               dataset_classes],
                                                  name="Input_Label")
             keep_prob = tf.placeholder(tf.float32, name="Keep_prob")
-            tf.add_to_collection(['global_step', 'inputs'], global_step)
-            tf.add_to_collection(['learn_rate', 'inputs'], learn_rate)
-            tf.add_to_collection(['input_tensor', 'inputs'], Input_Tensor_Image)
-            tf.add_to_collection(['label_tensor', 'inputs'], Input_Tensor_Labels)
-            tf.add_to_collection(['keep_prob', 'inputs'], keep_prob)
-        return Input_Tensor_Image, input_tensor, learn_rate
+        tf.add_to_collection('global_step', global_step)
+        tf.add_to_collection('learn_rate', learn_rate)
+        tf.add_to_collection('input_tensor', Input_Tensor_Image)
+        tf.add_to_collection('label_tensor', Input_Tensor_Labels)
+        tf.add_to_collection('keep_prob', keep_prob)
+        return Input_Tensor_Image, Input_Tensor_Labels, learn_rate
 
-    def build_outputs(x_image, num_conv, num_fc, num_outputs):
+    def build_outputs(self, x_image, num_conv, num_fc, num_outputs):
         with tf.variable_scope("conv_layers"):
-            x_image, num_features = BUILD_CONV_LAYERS(x_image, num_conv)
+            x_image, num_features = self.build_conv_layers(x_image, num_conv)
         with tf.variable_scope("Flat_layer"):
-            x_image, features = flatten_layer(x_image)
+            x_image, features = self.flatten_layer(x_image)
         with tf.variable_scope("FC_layers"):
-            final_layer = BUILD_FC_LAYER(x_image, num_fc, num_final=num_outputs)
+            final_layer = self.build_fc_layers(x_image, num_fc, num_final=num_outputs)
         return final_layer
 
-    def training_method(x_image, input_tensor, learn_rate):
+    def training_method(self, x_image, input_tensor, learn_rate):
         with tf.variable_scope("sophmax"):
             sophmax = tf.nn.softmax(x_image, name="sophmax")
         tf.add_to_collection("sophmax_layer", sophmax)
@@ -213,7 +211,9 @@ class Build_Model(object):
             train_drop_loss = tf.train.AdamOptimizer(learn_rate).minimize(loss)
         tf.add_to_collection("train_op", train_drop_loss);
 
-    def find_devices():
+
+    """
+    def find_devices(self, ):
         # do a known port scan for TF services
         # parse that for ip and port
         local_workers = []
@@ -261,3 +261,5 @@ class Build_Model(object):
                         # generic setup calls
                         init_op = tf.global_variables_initializer
                         merged = tf.summary.merge_all
+    """
+
